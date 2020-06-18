8c3fd39274b81041a4e7ae4944553d260e79ab3a
claudesimo1990 <claudesimo1990@gmail.com>
2020-06-18 23:48:18 +0200

    contact voyages 2

--- resources/views/livewire/contact-page.blade.php
+++ resources/views/livewire/contact-page.blade.php
@@ -7,13 +7,7 @@
                     <h3 class="quigo-subtitle">Email: {{$user->email}}</h3>
                 </div>
                 <div class="col-lg-6 text-center">
-                    <h3 class="quigo-subtitle">naissance: {{$profile->birthDay}}</h3>
-                    <h4 class="quigo-subtitle">ville: {{$profile->ville}}</h4>
-                    <h4 class="quigo-subtitle">pays: {{$profile->pays}}</h4>
-                    <h4 class="quigo-subtitle">rue: {{$profile->rue}}</h4>
-                    <h4 class="quigo-subtitle">phone: {{$profile->phone}}</h4>
-                    <h4 class="quigo-subtitle">verifier: {{$profile->isVerified ? 'oui' : 'non'}}</h4>
-                    <button wire:click="changepage">hallo</button>
+                    <h1>kommt die Infos hier !!!</h1>
                 </div>
             </div>
         </div>
