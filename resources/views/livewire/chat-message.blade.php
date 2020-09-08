89018ea0076006e37d37b2db2747d0cdabd8e756
claudesimo1990 <claudesimo1990@gmail.com>
2020-09-09 00:29:04 +0200

    add asset avatar chat

--- resources/views/livewire/chat-message.blade.php
+++ resources/views/livewire/chat-message.blade.php
@@ -17,10 +17,10 @@
                                 <li class="active itemclass" wire:click="findUser({{$user->id}})">
                                     <div class="d-flex bd-highlight">
                                         <div class="img_cont">
-                                            @if (Auth::user()->avatar_original)
-                                                <img src="{{Auth::user()->avatar_original}}" class="rounded-circle user_img">
+                                            @if ($user->avatar_original)
+                                                <img src="{{$user->avatar_original}}" class="rounded-circle user_img">
                                             @else
-                                                <img src="{{Auth::user()->avatar ? asset('storage/UsersAvatars/'.Auth::user()->avatar) :  asset('/img/logo.png') }}" class="rounded-circle user_img">
+                                                <img src="{{$user->avatar ? asset('storage/UsersAvatars/'.Auth::user()->avatar) :  asset('/img/logo.png') }}" class="rounded-circle user_img">
                                             @endif
                                             <span class="online_icon"></span>
                                         </div>
@@ -45,7 +45,7 @@
                     <div class="card-header msg_head">
                         <div class="d-flex bd-highlight">
                             <div class="img_cont">
-                                <img src="{{$fromUser['avatar']}}"class="rounded-circle user_img">
+                                <img src="{{asset('storage/UsersAvatars/' . $fromUser['avatar']}}"class="rounded-circle user_img">
                                 <span class="online_icon"></span>
                             </div>
                             <div class="user_info">
@@ -68,7 +68,7 @@
                             @if($message->user_id != $logUser->id)
                             <div class="d-flex justify-content-start mb-4">
                                 <div class="img_cont_msg">
-                                    <img src="{{$logUser->avatar}}" class="rounded-circle user_img_msg">
+                                    <img src="{{asset('storage/UsersAvatars/' . $logUser->avatar}}" class="rounded-circle user_img_msg">
                                 </div>
                                 <div class="msg_cotainer">
                                     <div class="text-message">{{$message->content}}</div>
@@ -82,7 +82,7 @@
                                     <span class="msg_time_send">8:55 AM, Today</span>
                                 </div>
                                 <div class="img_cont_msg">
-                                    <img src="{{$fromUser['avatar']}}"
+                                    <img src="{{asset('storage/UsersAvatars/' . $fromUser['avatar']}}"
                                         class="rounded-circle user_img_msg">
                                 </div>
                             </div>
