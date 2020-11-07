0bc72596d9ce8fe9a4b2821d76f05ffb97dadefa
claudesimo1990 <claudesimo1990@gmail.com>
2020-11-07 22:23:30 +0100

    add some changes to resolve bug

--- resources/views/emails/booking.blade.php
+++ resources/views/emails/booking.blade.php
@@ -1,8 +1,8 @@
 @component('mail::message')
 # Confirmation de Reservation
 
-Mr/me {{ $owner['name'] }} vous avez recue une nouvelle Reservationde de la part de
-{{ $user->name }}. pour confirmer cette reservation veuillez clicker sur
+Mr/me {{ $owner['name'] }} vous avez recue une nouvelle Reservationde de la part
+de {{ $user->name }}. pour confirmer cette reservation veuillez clicker sur
 le lien si dessous.
 
 @component('mail::button', ['url' => $url])
