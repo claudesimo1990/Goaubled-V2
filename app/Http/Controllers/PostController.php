2cfbd34c74b03ccf7f936adae056c34c0febf1fb
claudesimo1990 <45970298+claudesimo1990@users.noreply.github.com>
2020-11-08 08:24:08 +0100

    Dev claude (#39)
    * add app.js
    
    * add app.js
    
    * add some changes
    
    * add some changes to resolve bug
    
    * add some logik
    
    * postController
    
    * Delete app.js

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -122,8 +122,8 @@ class postController extends Controller
 
     public function booking(Request $request)
     {
-       Mail::to('claudesimo1990@gmail.com')->send(new BookingMail(auth()->user(), $request->get('owner'), route('confirm')));
-        // check for failures
+       Mail::to($request->get('owner')['email'])->send(new BookingMail(auth()->user(), $request->get('owner'), route('confirm')));
+
         if (Mail::failures()) {
             return response('error', 500);
         } else {
