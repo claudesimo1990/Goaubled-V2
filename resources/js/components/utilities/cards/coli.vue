2ad3884d5f569c4b2fa2b625260418b0006ad600
claudesimo1990 <claudesimo1990@gmail.com>
2020-11-20 10:40:00 +0100

    add nice2have

--- resources/js/components/utilities/cards/coli.vue
+++ resources/js/components/utilities/cards/coli.vue
@@ -33,10 +33,10 @@
                     <div class="col-xs-12 col-md-12 col-lg-6">
                         <h6 class="small text-left text-success">quelques photos de l'object</h6>
                         <div class="row">
-                            <div class="col-3 w-100 h-100 zoom"><img class="img-rounded img-responsive" width="100px" height="50px" :src="'storage/colis/' + item.colisPhoto"></div>
-                            <div class="col-3 w-100 h-100 zoom"><img class="img-rounded img-responsive" width="100px" height="50px" :src="'storage/colis/' + item.colisPhoto"></div>
-                            <div class="col-3 w-100 h-100 zoom"><img class="img-rounded img-responsive" width="100px" height="50px" :src="'storage/colis/' + item.colisPhoto"></div>
-                            <div class="col-3 w-100 h-100 zoom"><img class="img-rounded img-responsive" width="100px" height="50px" :src="'storage/colis/' + item.colisPhoto"></div>
+                            <div class="col-3 w-100 h-100 zoom"><img class="img-rounded img-responsive" width="100px" height="50px" :src="'/storage/colis/' + item.colisPhoto"></div>
+                            <div class="col-3 w-100 h-100 zoom"><img class="img-rounded img-responsive" width="100px" height="50px" :src="'/storage/colis/' + item.colisPhoto"></div>
+                            <div class="col-3 w-100 h-100 zoom"><img class="img-rounded img-responsive" width="100px" height="50px" :src="'/storage/colis/' + item.colisPhoto"></div>
+                            <div class="col-3 w-100 h-100 zoom"><img class="img-rounded img-responsive" width="100px" height="50px" :src="'/storage/colis/' + item.colisPhoto"></div>
                         </div>
                     </div>
                     <div class="col-xs-12 col-md-12 col-lg-6 pt-5">
