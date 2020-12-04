b7b95a9f6c0a133ec8a5d44db019ee9812337891
claudesimo1990 <claudesimo1990@gmail.com>
2020-12-04 12:23:47 +0100

    zwischen Stand

--- resources/js/components/howPostPack.vue
+++ resources/js/components/howPostPack.vue
@@ -1,60 +1,76 @@
 <template>
-<!-- expedier un coli -->
-<section class="our-blog p-0 m-0 bg-silver">
-    <div class="container pb-5 pt-5">
-        <div class="section-header">
-            <h2 class="text-center title__annonce">Souhaitez vous envoyer un colis?</h2>
-        </div>
-        <div class="row border my-3 py-3">
-            <div class="col-md-2">    
-                 <div class="numberCircle">Step 1</div>
+    <!-- expedier un coli -->
+    <section class="our-blog p-0 m-0 bg-silver">
+        <div class="container pb-5 pt-5">
+            <div class="section-header">
+                <h2 class="text-center title__annonce">
+                    Souhaitez vous envoyer un colis?
+                </h2>
             </div>
+            <div class="row border my-3 py-3">
+                <div class="col-md-2">
+                    <div class="numberCircle">Step 1</div>
+                </div>
                 <div class="col-md-10">
                     <h4 class="border-2">Expedier un colis</h4>
-                <p>Remplisser le Formulaire. En inserant la date, lieu depart et d'arrivve, le nombre de kilo a envoyer et le prix par kilo. Ainsi que le prix du courrier.
-                 Donnez plus d'information sur la nature du colis</p>        
+                    <p>
+                        Remplisser le Formulaire. En inserant la date, lieu
+                        depart et d'arrivve, le nombre de kilo a envoyer et le
+                        prix par kilo. Ainsi que le prix du courrier. Donnez
+                        plus d'information sur la nature du colis
+                    </p>
                 </div>
-        </div>
-            <!-- 2 -->
-          <div class="row border my-3 py-3">
-            <div class="col-md-2">    
-                 <div class="numberCircle">Step 2</div>
             </div>
+            <!-- 2 -->
+            <div class="row border my-3 py-3">
+                <div class="col-md-2">
+                    <div class="numberCircle">Step 2</div>
+                </div>
                 <div class="col-md-10">
                     <h4 class="border-2">Se proposer</h4>
-                     <p>Un voyageur se propose de transporter votre colis et vous recevez un message par mail. Si vous etez d' accord, confirmez ca demande</p>
+                    <p>
+                        Un voyageur se propose de transporter votre colis et
+                        vous recevez un message par mail. Si vous etez d'
+                        accord, confirmez ca demande
+                    </p>
                 </div>
-        </div>
-
-        <!-- 3 -->
-           <div class="row border my-3 py-3">
-            <div class="col-md-2">    
-                 <div class="numberCircle">Step 3</div>
             </div>
+
+            <!-- 3 -->
+            <div class="row border my-3 py-3">
+                <div class="col-md-2">
+                    <div class="numberCircle">Step 3</div>
+                </div>
                 <div class="col-md-10">
                     <h4 class="border-2">Depot du colis</h4>
-                  <p>Vous remettez votre colis au voyageur et faite une liste de tous les colis à transporter. Ainsi que une procuration, qui atteste que transporteur achemine les produits listes en votre nom.</p>
+                    <p>
+                        Vous remettez votre colis au voyageur et faite une liste
+                        de tous les colis à transporter. Ainsi que une
+                        procuration, qui atteste que transporteur achemine les
+                        produits listes en votre nom.
+                    </p>
                 </div>
-        </div>
-        <!-- 4 -->
-           <div class="row border my-3 py-3">
-            <div class="col-md-2">    
-                 <div class="numberCircle">Step 4</div>
             </div>
+            <!-- 4 -->
+            <div class="row border my-3 py-3">
+                <div class="col-md-2">
+                    <div class="numberCircle">Step 4</div>
+                </div>
                 <div class="col-md-10">
                     <h4 class="border-2">Paiment</h4>
-                    <p>Après le trajet et la remise du colis, votre argent debite sera trafere sur le compte PayPal du voyageur. </p>
+                    <p>
+                        Après le trajet et la remise du colis, votre argent
+                        debite sera trafere sur le compte PayPal du voyageur.
+                    </p>
                 </div>
+            </div>
+            <!--  -->
         </div>
-        <!--  -->
-    </div>
-</section>
+    </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss">
--- resources/views/App/profile.blade.php
+++ resources/views/App/profile.blade.php
@@ -3,6 +3,8 @@
 
 @section('content')
 
+<h1>salut</h1>
+
     <profile-component 
     :current-user="{{ auth()->user() }}"
     ></profile-component>
