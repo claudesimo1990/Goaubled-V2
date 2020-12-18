5a3d97c5b0698585fab6d8dec59c4ac22b8ff76c
claudesimo1990 <claudesimo1990@gmail.com>
2020-12-18 19:46:50 +0100

    add admin factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,14 +7,9 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => 'admin',
-        'email' => $faker->email,
-        'email_verified_at' => $faker->dateTime(),
-        'google_id' => $faker->word,
-        'facebbook_id' => $faker->word,
-        'avatar' => $faker->word,
-        'avatar_original' => $faker->word,
+        'name' => 'Admin',
+        'email' => 'admin@admin.com',
         'password' => '$2y$10$gBSyzwffVCN/nmls9eQZd.egmGQcFfv4hPGyqjeahUR7G5nqyt6ke', //password
-        'is_admin' => 0
+        'is_admin' > 1
     ];
 });
