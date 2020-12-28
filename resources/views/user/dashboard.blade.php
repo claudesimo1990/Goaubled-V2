25378305e46c6150afeb2c7be58890524689f447
claudesimo1990 <claudesimo1990@gmail.com>
2020-12-28 14:53:35 +0100

    filter users chat

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -100,12 +100,14 @@
         <div class="container-fluid">
 
           <!-- Page Heading -->
-          <div class="d-sm-flex align-items-center justify-content-between">
+          <div class="d-sm-flex align-items-center justify-content-between mt-4">
             <h4 class="mb-0 text-gray-800">@yield('dashboard_title')</h4>
           </div>
 
-        <!-- Content Row -->
-          @yield('dashbord_content')
+        <div class="mt-4">
+           <!-- Content Row -->
+           @yield('dashbord_content')
+        </div>
 
         </div>
         <!-- /.container-fluid -->
