72500f9f951f1d3821549668580213d060ee86a6
claudesimo1990 <claudesimo1990@gmail.com>
2020-12-29 21:14:39 +0100

    add helpers

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\post;
-use App\User;
-use App\Events\PresentEvent;
+use app\post;
+use app\User;
+use app\Events\PresentEvent;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
