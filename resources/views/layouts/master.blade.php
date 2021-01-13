64bcf380021d56ceb9cb85393aa00ae3ec7e959e
claudesimo1990 <claudesimo1990@gmail.com>
2021-01-13 19:08:42 +0100

    add https

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,10 +2,11 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>goaubled</title>
+    <title>Goaubled</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
@@ -14,11 +15,6 @@
     <link href="{{asset('/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
     <link href="{{asset('/lib/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
     <link href="{{asset('/lib/animate/animate.min.css')}}" rel="stylesheet">
-    <link href="{{asset('/lib/ionicons/css/ionicons.min.css')}}" rel="stylesheet">
-    <link href="{{asset('/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
-    <link href="{{asset('/lib/magnific-popup/magnific-popup.css')}}" rel="stylesheet">
-    <link href="{{asset('/lib/ionicons/css/ionicons.min.css')}}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
     @yield('css')
     <link href="{{'/css/app.css'}}" rel="stylesheet">
 </head>
@@ -45,24 +41,15 @@
     </div>
 
 <!-- JavaScript Libraries -->
-<script
-  src="https://code.jquery.com/jquery-3.5.1.min.js"
-  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-  crossorigin="anonymous">
-</script>
 @yield('js')
 <script src="{{ asset("/js/app.js") }}"></script>
-<script defer src="{{ asset('/lib/superfish/superfish.min.js') }}"></script>
 <script defer src="{{ asset('/lib/wow/wow.min.js') }}"></script>
-<script defer src="{{ asset('/lib/owlcarousel/owl.carousel.min.js') }}"></script>
-<script defer src="{{ asset('/lib/magnific-popup/magnific-popup.min.js') }}"></script>
 <script defer src="{{ asset('/lib/sticky/sticky.js') }}"></script>
 <!-- Template Main Javascript File -->
 <script defer src="{{ asset("/js/main.js") }}"></script>
-<script defer src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/dropzone.js"></script>
 <script defer src="{{ asset('./js/script.js') }}"></script>
 @stack('scripts')
 @include('flashy::message')
-    
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Mail;
 
 Route::get('/','AppController@index')->name('accueil');
 Route::get('/howItWork','AppController@howItWork')->name('howItWork');
@@ -51,15 +50,18 @@ Route::post('resetAllunreadNotifications', 'NotificationsController@update')->mi
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/profile/{user}', 'ProfileController@show')->name('profile.show');
 //User Dashboard
-Route::get('/profile/{user}/chat', 'ContactController@getChat')->name('user.message')->middleware('auth');
-Route::get('/profile/{user}/notifications', 'ContactController@notifications')->name('user.notifications')->middleware('auth');
-Route::get('/profile/{user}/profile', 'ProfileController@create')->name('user.profile')->middleware('auth');
-Route::post('/profile/{user}/profile', 'ProfileController@update');
-Route::get('/profile/{user}/reservations', 'userActionController@reservations')->name('user.reservations')->middleware('auth');
-Route::get('/profile/{user}/factures', 'userActionController@factures')->name('user.factures')->middleware('auth');
-Route::get('/profile/{user}/demandes', 'userActionController@demandes')->name('user.demandes')->middleware('auth');
-Route::get('/profile/{user}/travels', 'userActionController@travels')->name('user.travels')->middleware('auth');
-Route::get('/profile/{user}/packs', 'userActionController@packs')->name('user.packs')->middleware('auth');
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/profile/{user}/chat', 'ContactController@getChat')->name('user.message')->middleware('auth');
+    Route::get('/profile/{user}/notifications', 'ContactController@notifications')->name('user.notifications')->middleware('auth');
+    Route::get('/profile/{user}/profile', 'ProfileController@create')->name('user.profile')->middleware('auth');
+    Route::post('/profile/{user}/profile', 'ProfileController@update');
+    Route::get('/profile/{user}/reservations', 'userActionController@reservations')->name('user.reservations')->middleware('auth');
+    Route::get('/profile/{user}/factures', 'userActionController@factures')->name('user.factures')->middleware('auth');
+    Route::get('/profile/{user}/demandes', 'userActionController@demandes')->name('user.demandes')->middleware('auth');
+    Route::get('/profile/{user}/travels', 'userActionController@travels')->name('user.travels')->middleware('auth');
+    Route::get('/profile/{user}/packs', 'userActionController@packs')->name('user.packs')->middleware('auth');
+});
+
 Route::get('/conversation/{id}', 'ContactController@getMessagesFor');
 Route::post('/conversation/send', 'ContactController@send');
 
@@ -70,10 +72,11 @@ Route::get('/callback', 'Auth\LoginController@handleProviderCallback');
 Route::get('/facebook', 'facebooklogController@redirect')->name('facebook');
 
  // admin
-
-route::get('/admin/home','Admin\HomeController@home')->name('admin.home')->middleware('is_admin');
-route::get('/admin/login','Admin\AuthController@login')->name('admin.login');
-route::post('/admin/login','Admin\AuthController@attemptAdmin');
+Route::namespace('admin',function(){
+    route::get('/home','Admin\HomeController@home')->name('admin.home')->middleware('is_admin');
+    route::get('/login','Admin\AuthController@login')->name('admin.login');
+    route::post('/login','Admin\AuthController@attemptAdmin');
+});
 
 Route::group(['middleware' => ['is_admin']], function () {
 
@@ -90,5 +93,5 @@ Route::group(['middleware' => ['is_admin']], function () {
     Route::get('/admin/colis', 'Admin\ColisController@index')->name('admin.colis.index')->middleware('is_admin');
     Route::get('/admin/colis/accept/{travel}', 'Admin\ColisController@acceptPost')->name('admin.coli.validate')->middleware('is_admin');
     Route::get('/admin/colis/reject/{travel}', 'Admin\ColisController@rejectPost')->name('admin.coli.reject')->middleware('is_admin');
-    
+
 });
