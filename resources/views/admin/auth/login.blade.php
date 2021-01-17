7a4d0b2aa5c0512e088c5bc4aa862859703b567f
claudesimo1990 <claudesimo1990@gmail.com>
2021-01-17 16:02:12 +0100

    add login

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,110 +1,42 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Admin</title>
-</head>
+@extends('admin.auth.layouts')
 
-<style>
 
-body{
-    margin: 0;
-    font-size: .9rem;
-    font-weight: 400;
-    line-height: 1.6;
-    color: #212529;
-    text-align: left;
-    background-color: #f5f8fa !important;
-}
+@section('content')
 
-.navbar-laravel
-{
-    box-shadow: 0 2px 4px rgba(0,0,0,.04);
-}
-
-.navbar-brand , .nav-link, .my-form, .login-form
-{
-    font-family: Raleway, sans-serif;
-}
-
-.my-form
-{
-    padding-top: 1.5rem;
-    padding-bottom: 1.5rem;
-}
-
-.my-form .row
-{
-    margin-left: 0;
-    margin-right: 0;
-}
-
-.login-form
-{
-    padding-top: 1.5rem;
-    padding-bottom: 1.5rem;
-}
-
-.login-form .row
-{
-    margin-left: 0;
-    margin-right: 0;
-}
-</style>
-<body class="bg-gray border-admin__background">
-<main class="login-form">
-    <div class="cotainer my-5 py-5">
-    </div>
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-head py-0">
-                        <h4 class="text-center">Admin GoauBled</h4>
+    <div class="container w-100 h-100">
+        <div class="d-flex justify-content-center align-items-center">
+            <div class="jumbotron">
+                <h1 class="display-4">Login | Admin</h1>
+                @if(session()->has('message'))
+                    <div class="alert alert-danger">
+                        {{ session('message') }}
                     </div>
-                    <div class="card-body border">
-                        @if(session()->has('message'))
-                        <div class="alert alert-danger">
-                            {{ session('message') }}
+                @endif
+                @if (session()->has('error'))
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
+                    </div>
+                @endif
+                <div class="">
+                    <form action="{{ route('admin.login') }}" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" name="email" id="email" placeholder="entrer votre email">
+                            @error('email')<span class="text-left text-red small">{{ $message }}</span>@enderror
                         </div>
-                        @endif
-                        @if (session()->has('error'))
-                            <div class="alert alert-danger">
-                                {{ session('error') }}
-                            </div>
-                        @endif
-                        <form action="{{ route('admin.login') }}" method="POST">
-                            @csrf
-                            <div class="form-group row">
-                                <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-                                <div class="col-md-6">
-                                    <input type="text" name="email" class="form-control" id="exampleInputPassword1" placeholder="email">
-                                    @error('email')<span class="text-left text-red small">{{ $message }}</span>@enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-                                <div class="col-md-6">
-                                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="mot de passe">
-                                    @error('password')<span class="text-left text-red small">{{ $message }}</span>@enderror
-                                </div>
-                            </div>
-
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-secondary">
-                                    login
-                                </button>
-                            </div>
+                        <div class="form-group">
+                            <label for="email">Password</label>
+                            <input type="password" class="form-control" name="password" id="email" placeholder="entrer votre Mot de passe">
+                            @error('password')<span class="text-left text-red small">{{ $message }}</span>@enderror
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success">login</button>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-    </div>
-</main>
-</body>
-</html>
+
+    @endsection
