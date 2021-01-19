b4d9a691f243b2db19e3c6c1c14263760337143b
claudesimo1990 <claudesimo1990@gmail.com>
2021-01-19 21:25:42 +0100

    add login with facebook and google

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,7 +11,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => 'admin@admin.com',
         'email_verified_at' => $faker->dateTime(),
         'google_id' => $faker->word,
-        'facebbook_id' => $faker->word,
+        'facebook_id' => $faker->word,
         'avatar' => $faker->imageUrl($width = 640, $height = 480),
         'avatar_original' => $faker->word,
         'password' => '$2y$10$gBSyzwffVCN/nmls9eQZd.egmGQcFfv4hPGyqjeahUR7G5nqyt6ke', //password
