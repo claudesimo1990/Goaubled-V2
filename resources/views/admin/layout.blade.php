2ffbc83f397023863970d094731852dfa9535798
claudesimo1990 <claudesimo1990@gmail.com>
2021-01-22 00:56:01 +0100

    changes chat

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -3,17 +3,17 @@
 
 <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Goaubled</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
 
-  <title>Goaubled-Admin</title>
+    <title>Goaubled-Admin</title>
 
   <!-- Custom fonts for this template-->
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
